3695a9084f4e1796f2bacbb9446862bb65b6b4e9
Aaron <aaroner@yahoo.com>
2019-03-17 18:42:41 -0500

    Starting on js functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,20 @@
-// This file is unused for now
-console.log(123);
-// This is from the testBranch
+// Selectors
+const neck = document.querySelector('.neck');
+const frets = document.querySelectorAll('.fret');
+const fretsArr = [...frets];
+
+// Event Listeners
+fretsArr.forEach(fret => fret.addEventListener('mouseenter', hoverHighlight));
+// Second eventlistener for mouseleave to remove highlight?
+
+// Functions
+function hoverHighlight() {
+  // Get current item
+
+  // Loop over and check
+  frets.forEach(fret => {
+    if (fret.innerText.includes('E')) {
+      fret.classList.add('highlight');
+    }
+  });
+}
